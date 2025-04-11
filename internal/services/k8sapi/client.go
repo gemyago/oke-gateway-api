5371443fec1a6c80daff0d10763c9b29c93d42ee
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-04-11 23:44:41 +0200

    Init and register client.

---
+++ internal/services/k8sapi/client.go
@@ -0,0 +1,31 @@
+package k8sapi
+
+import (
+	"flag"
+	"path/filepath"
+
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func newConfig() (*rest.Config, error) {
+	// create new config
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String(
+			"kubeconfig",
+			filepath.Join(home, ".kube", "config"),
+			"(optional) absolute path to the kubeconfig file",
+		)
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+
+	return clientcmd.BuildConfigFromFlags("", *kubeconfig)
+}
+
+func newClient(config *rest.Config) (client.Client, error) {
+	return client.New(config, client.Options{})
+}
---
+++ internal/services/k8sapi/register.go
@@ -0,0 +1,13 @@
+package k8sapi
+
+import (
+	"github.com/gemyago/oke-gateway-api/internal/di"
+	"go.uber.org/dig"
+)
+
+func Register(container *dig.Container) error {
+	return di.ProvideAll(container,
+		newClient,
+		newClient,
+	)
+}
