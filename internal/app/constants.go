52749d57e62afce762459f6a889fbf71875c5881
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-04-14 09:13:36 +0200

    Adding another reason.

--- internal/app/constants.go
+++ internal/app/constants.go
@@ -3,8 +3,12 @@ package app
 const (
 	// AcceptedConditionType is the condition type for resource acceptance.
 	AcceptedConditionType = "Accepted"
+
 	// AcceptedConditionReason is the reason for the Accepted condition when true.
 	AcceptedConditionReason = "Accepted"
+
+	// InvalidResourceConfigurationReason is the reason for the InvalidResourceConfiguration condition.
+	InvalidResourceConfigurationReason = "InvalidResourceConfiguration"
 
 	// ControllerClassName is the name of the controller managing resources.
 	ControllerClassName = "oke-gateway-api.oraclecloud.com/oke-gateway-controller"
