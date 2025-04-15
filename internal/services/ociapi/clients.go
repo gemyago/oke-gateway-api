3323ff5cb98387c9f31ebb0aaa830af790870008
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-04-16 00:10:25 +0200

    Register stuff.

--- internal/services/ociapi/clients.go
+++ internal/services/ociapi/clients.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/oracle/oci-go-sdk/v65/common"
 	"github.com/oracle/oci-go-sdk/v65/loadbalancer"
+	"github.com/oracle/oci-go-sdk/v65/workrequests"
 )
 
 func newLoadBalancerClient(
@@ -13,6 +14,16 @@ func newLoadBalancerClient(
 	client, err := loadbalancer.NewLoadBalancerClientWithConfigurationProvider(configProvider)
 	if err != nil {
 		return loadbalancer.LoadBalancerClient{}, fmt.Errorf("failed to create load balancer client: %w", err)
+	}
+	return client, nil
+}
+
+func newWorkRequestsClient(
+	configProvider common.ConfigurationProvider,
+) (workrequests.WorkRequestClient, error) {
+	client, err := workrequests.NewWorkRequestClientWithConfigurationProvider(configProvider)
+	if err != nil {
+		return workrequests.WorkRequestClient{}, fmt.Errorf("failed to create work requests client: %w", err)
 	}
 	return client, nil
 }
--- internal/services/ociapi/register.go
+++ internal/services/ociapi/register.go
@@ -2,6 +2,7 @@ package ociapi
 
 import (
 	"github.com/gemyago/oke-gateway-api/internal/di"
+	"github.com/oracle/oci-go-sdk/v65/workrequests"
 	"go.uber.org/dig"
 )
 
@@ -9,5 +10,8 @@ func Register(container *dig.Container) error {
 	return di.ProvideAll(container,
 		newConfigProvider,
 		newLoadBalancerClient,
+		newWorkRequestsClient,
+		NewWorkRequestsWatcher,
+		func(c workrequests.WorkRequestClient) workRequestsClient { return c },
 	)
 }
