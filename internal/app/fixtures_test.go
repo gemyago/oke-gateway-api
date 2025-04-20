378d822ade639b6c1976913d23ad6b171ebefe3f
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-04-20 21:45:52 +0200

    Random route.

--- internal/app/fixtures_test.go
+++ internal/app/fixtures_test.go
@@ -150,3 +150,12 @@ func randomOCILoadBalancerWithRandomListenersOpt() randomOCILoadBalancerOpt {
 		}
 	}
 }
+
+func randomHTTPRoute() *gatewayv1.HTTPRoute {
+	return &gatewayv1.HTTPRoute{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      faker.DomainName(),
+			Namespace: faker.Username(),
+		},
+	}
+}
