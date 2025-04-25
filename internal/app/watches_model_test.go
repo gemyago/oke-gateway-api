23cbf080c4c4f7f05eba24f077382203ec81442f
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-04-25 08:34:46 +0200

    Initial failing test.

--- internal/app/watches_model_test.go
+++ internal/app/watches_model_test.go
@@ -1,10 +1,13 @@
 package app
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/gemyago/oke-gateway-api/internal/diag"
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/require"
+	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
 
 func TestWatchesModel(t *testing.T) {
@@ -20,12 +23,54 @@ func TestWatchesModel(t *testing.T) {
 			deps := makeMockDeps(t)
 			model := NewWatchesModel(deps)
 
-			httpRoute := makeRandomHTTPRoute()
+			refs1 := []gatewayv1.HTTPBackendRef{
+				makeRandomBackendRef(),
+				makeRandomBackendRef(),
+				makeRandomBackendRef(),
+			}
+
+			refs2 := []gatewayv1.HTTPBackendRef{
+				makeRandomBackendRef(),
+				makeRandomBackendRef(),
+				makeRandomBackendRef(),
+			}
+
+			refs3 := []gatewayv1.HTTPBackendRef{
+				makeRandomBackendRef(),
+				makeRandomBackendRef(),
+				makeRandomBackendRef(),
+			}
+
+			httpRoute := makeRandomHTTPRoute(
+				randomHTTPRouteWithRandomRulesOpt(
+					randomHTTPRouteRule(
+						randomHTTPRouteRuleWithRandomBackendRefsOpt(refs1...),
+					),
+					randomHTTPRouteRule(
+						randomHTTPRouteRuleWithRandomBackendRefsOpt(refs2...),
+					),
+				),
+				randomHTTPRouteWithRandomRulesOpt(
+					randomHTTPRouteRule(
+						randomHTTPRouteRuleWithRandomBackendRefsOpt(refs3...),
+					),
+				),
+			)
+
+			allRefs := make([]gatewayv1.HTTPBackendRef, 0, len(refs1)+len(refs2)+len(refs3))
+			allRefs = append(allRefs, refs1...)
+			allRefs = append(allRefs, refs2...)
+			allRefs = append(allRefs, refs3...)
+			wantIndices := lo.Map(allRefs, func(ref gatewayv1.HTTPBackendRef, _ int) string {
+				return fmt.Sprintf("%v/%v",
+					ref.BackendObjectReference.Namespace,
+					ref.BackendObjectReference.Name,
+				)
+			})
 
 			result := model.indexHTTPRouteByBackendService(&httpRoute)
 
-			// Assert
-			require.Nil(t, result, "Expected nil result from stub implementation")
+			require.ElementsMatch(t, wantIndices, result)
 		})
 	})
 }
