1deb4e0d25ccd5c473dbc8f9778401e2ef80f5ee
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-04-26 08:38:09 +0200

    Fix tests.

--- internal/app/httpbackend_model_test.go
+++ internal/app/httpbackend_model_test.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/gemyago/oke-gateway-api/internal/diag"
@@ -27,23 +26,23 @@ func TestHTTPBackendModel(t *testing.T) {
 		}
 	}
 
-	t.Run("syncBackendEndpoints", func(t *testing.T) {
-		t.Run("update backend sets", func(t *testing.T) {
+	t.Run("syncRouteBackendRuleEndpoints", func(t *testing.T) {
+		t.Run("update backend set", func(t *testing.T) {
 			deps := newMockDeps(t)
 			model := newHTTPBackendModel(deps)
-
-			refs1 := []gatewayv1.HTTPBackendRef{
-				makeRandomBackendRef(),
-				makeRandomBackendRef(),
-			}
 
 			refs2 := []gatewayv1.HTTPBackendRef{
 				makeRandomBackendRef(),
 				makeRandomBackendRef(),
 			}
 
-			rule1 := makeRandomHTTPRouteRule(randomHTTPRouteRuleWithRandomBackendRefsOpt(refs1...))
-			rule2 := makeRandomHTTPRouteRule(randomHTTPRouteRuleWithRandomBackendRefsOpt(refs2...))
+			rule1 := makeRandomHTTPRouteRule(randomHTTPRouteRuleWithRandomBackendRefsOpt(
+				makeRandomBackendRef(),
+				makeRandomBackendRef(),
+			))
+			rule2 := makeRandomHTTPRouteRule(randomHTTPRouteRuleWithRandomBackendRefsOpt(
+				refs2...,
+			))
 
 			httpRoute := makeRandomHTTPRoute(
 				randomHTTPRouteWithRandomRulesOpt(rule1, rule2),
@@ -51,11 +50,7 @@ func TestHTTPBackendModel(t *testing.T) {
 
 			config := makeRandomGatewayConfig()
 
-			allRefs := make([]gatewayv1.HTTPBackendRef, 0, len(refs1)+len(refs2))
-			allRefs = append(allRefs, refs1...)
-			allRefs = append(allRefs, refs2...)
-
-			endpointSlicesByRef := lo.SliceToMap(allRefs,
+			endpointSlicesByRef := lo.SliceToMap(refs2,
 				func(ref gatewayv1.HTTPBackendRef) (string, discoveryv1.EndpointSlice) {
 					return string(ref.BackendObjectReference.Name), makeRandomEndpointSlice(
 						randomEndpointSliceWithEndpointsOpt(),
@@ -64,11 +59,11 @@ func TestHTTPBackendModel(t *testing.T) {
 
 			mockK8sClient, _ := deps.K8sClient.(*Mockk8sClient)
 
-			for _, ref := range allRefs {
+			for _, ref := range refs2 {
 				mockK8sClient.EXPECT().List(
 					t.Context(),
 					mock.Anything,
-					client.MatchingFields{
+					client.MatchingLabels{
 						discoveryv1.LabelServiceName: string(ref.BackendObjectReference.Name),
 					},
 				).RunAndReturn(func(_ context.Context, ol client.ObjectList, _ ...client.ListOption) error {
@@ -79,47 +74,45 @@ func TestHTTPBackendModel(t *testing.T) {
 				}).Once()
 			}
 
-			for index, rule := range httpRoute.Spec.Rules {
-				var wantBackends []loadbalancer.BackendDetails
-				for _, backendRef := range rule.BackendRefs {
-					refSlice := endpointSlicesByRef[string(backendRef.BackendObjectReference.Name)]
-					for _, endpoint := range refSlice.Endpoints {
-						port := int32(*backendRef.BackendObjectReference.Port)
-						wantBackends = append(wantBackends, loadbalancer.BackendDetails{
-							Port:      lo.ToPtr(int(port)),
-							IpAddress: &endpoint.Addresses[0],
-						})
-					}
+			var wantBackends []loadbalancer.BackendDetails
+			for _, backendRef := range rule2.BackendRefs {
+				refSlice := endpointSlicesByRef[string(backendRef.BackendObjectReference.Name)]
+				for _, endpoint := range refSlice.Endpoints {
+					port := int32(*backendRef.BackendObjectReference.Port)
+					wantBackends = append(wantBackends, loadbalancer.BackendDetails{
+						Port:      lo.ToPtr(int(port)),
+						IpAddress: &endpoint.Addresses[0],
+					})
 				}
-
-				backendSetName := backendSetName(httpRoute, rule, index)
-
-				wantOperationID := faker.UUIDHyphenated()
-				mockOciLoadBalancerClient, _ := deps.OciLoadBalancerClient.(*MockociLoadBalancerClient)
-				mockOciLoadBalancerClient.EXPECT().UpdateBackendSet(
-					t.Context(),
-					mock.MatchedBy(func(req loadbalancer.UpdateBackendSetRequest) bool {
-						fmt.Println("req", *req.LoadBalancerId, *req.BackendSetName, backendSetName)
-
-						return lo.NoneBy(
-							[]bool{
-								assert.Equal(t, *req.LoadBalancerId, config.Spec.LoadBalancerID),
-								// assert.Equal(t, *req.BackendSetName, backendSetName),
-							},
-							func(b bool) bool { return !b },
-						)
-					}),
-				).Return(loadbalancer.UpdateBackendSetResponse{
-					OpcWorkRequestId: &wantOperationID,
-				}, nil).Once()
-
-				mockWorkRequestsWatcher, _ := deps.WorkRequestsWatcher.(*MockworkRequestsWatcher)
-				mockWorkRequestsWatcher.EXPECT().WaitFor(t.Context(), wantOperationID).Return(nil).Once()
 			}
 
-			err := model.syncRouteBackendEndpoints(t.Context(), syncRouteBackendEndpointsParams{
+			backendSetName := backendSetName(httpRoute, rule2, 1)
+
+			wantOperationID := faker.UUIDHyphenated()
+			mockOciLoadBalancerClient, _ := deps.OciLoadBalancerClient.(*MockociLoadBalancerClient)
+			mockOciLoadBalancerClient.EXPECT().UpdateBackendSet(
+				t.Context(),
+				mock.MatchedBy(func(req loadbalancer.UpdateBackendSetRequest) bool {
+					return lo.NoneBy(
+						[]bool{
+							assert.Equal(t, *req.LoadBalancerId, config.Spec.LoadBalancerID),
+							assert.Equal(t, *req.BackendSetName, backendSetName),
+							assert.Equal(t, wantBackends, req.Backends),
+						},
+						func(b bool) bool { return !b },
+					)
+				}),
+			).Return(loadbalancer.UpdateBackendSetResponse{
+				OpcWorkRequestId: &wantOperationID,
+			}, nil).Once()
+
+			mockWorkRequestsWatcher, _ := deps.WorkRequestsWatcher.(*MockworkRequestsWatcher)
+			mockWorkRequestsWatcher.EXPECT().WaitFor(t.Context(), wantOperationID).Return(nil).Once()
+
+			err := model.syncRouteBackendRuleEndpoints(t.Context(), syncRouteBackendRuleEndpointsParams{
 				httpRoute: httpRoute,
 				config:    config,
+				ruleIndex: 1,
 			})
 
 			require.NoError(t, err)
