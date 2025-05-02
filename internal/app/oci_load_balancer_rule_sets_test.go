1a1ef2ee86cb5b57e186ba63939cdc485a08af11
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-05-02 18:41:39 +0200

    Iterating on mapping.

--- internal/app/oci_load_balancer_rule_sets_test.go
+++ internal/app/oci_load_balancer_rule_sets_test.go
@@ -14,210 +14,116 @@ import (
 
 func TestOciLoadBalancerRuleSetsImpl(t *testing.T) {
 	t.Run("mapHTTPRouteMatchToCondition", func(t *testing.T) {
-		r := newOciLoadBalancerRuleSets()
-
-		tests := []struct {
+		type testCase struct {
 			name        string
 			match       gatewayv1.HTTPRouteMatch
 			want        string
 			wantErrIs   error
 			wantErrText string
-		}{
-			{
-				name: "exact path match",
-				match: gatewayv1.HTTPRouteMatch{
-					Path: &gatewayv1.HTTPPathMatch{
-						Type:  lo.ToPtr(gatewayv1.PathMatchExact),
-						Value: lo.ToPtr("/foo/bar"),
-					},
-				},
-				want: `http.request.url.path eq '/foo/bar'`,
-			},
-			{
-				name: "prefix path match",
-				match: gatewayv1.HTTPRouteMatch{
-					Path: &gatewayv1.HTTPPathMatch{
-						Type:  lo.ToPtr(gatewayv1.PathMatchPathPrefix),
-						Value: lo.ToPtr("/baz"),
-					},
-				},
-				want: `http.request.url.path sw '/baz'`,
-			},
-			{
-				name: "exact header match",
-				match: gatewayv1.HTTPRouteMatch{
-					Headers: []gatewayv1.HTTPHeaderMatch{
-						{
-							Type:  lo.ToPtr(gatewayv1.HeaderMatchExact),
-							Name:  "X-My-Header",
-							Value: "value1",
-						},
-					},
-				},
-				want: `http.request.headers['X-My-Header'] eq 'value1'`,
-			},
-			{
-				name: "multiple exact header matches",
-				match: gatewayv1.HTTPRouteMatch{
-					Headers: []gatewayv1.HTTPHeaderMatch{
-						{
-							Type:  lo.ToPtr(gatewayv1.HeaderMatchExact),
-							Name:  "X-Header-1",
-							Value: "val1",
-						},
-						{
-							Type:  lo.ToPtr(gatewayv1.HeaderMatchExact),
-							Name:  "X-Header-2",
-							Value: "val2",
-						},
-					},
-				},
-				want: `http.request.headers['X-Header-1'] eq 'val1' and http.request.headers['X-Header-2'] eq 'val2'`,
-			},
-			{
-				name: "exact path and exact header match",
-				match: gatewayv1.HTTPRouteMatch{
-					Path: &gatewayv1.HTTPPathMatch{
-						Type:  lo.ToPtr(gatewayv1.PathMatchExact),
-						Value: lo.ToPtr("/login"),
-					},
-					Headers: []gatewayv1.HTTPHeaderMatch{
-						{
-							Type:  lo.ToPtr(gatewayv1.HeaderMatchExact),
-							Name:  "Content-Type",
-							Value: "application/json",
-						},
-					},
-				},
-				want: `http.request.url.path eq '/login' and http.request.headers['Content-Type'] eq 'application/json'`,
-			},
-			{
-				name: "prefix path and multiple exact header matches",
-				match: gatewayv1.HTTPRouteMatch{
-					Path: &gatewayv1.HTTPPathMatch{
-						Type:  lo.ToPtr(gatewayv1.PathMatchPathPrefix),
-						Value: lo.ToPtr("/api/v1"),
-					},
-					Headers: []gatewayv1.HTTPHeaderMatch{
-						{
-							Type:  lo.ToPtr(gatewayv1.HeaderMatchExact),
-							Name:  "Authorization",
-							Value: "Bearer " + faker.Jwt(),
-						},
-						{
-							Type:  lo.ToPtr(gatewayv1.HeaderMatchExact),
-							Name:  "X-Request-ID",
-							Value: faker.UUIDHyphenated(),
-						},
-					},
-				},
-				// Note: OCI condition generation logic should handle quoting inner single quotes if necessary
-				want: fmt.Sprintf(
-					`http.request.url.path sw '/api/v1' and `+
-						`http.request.headers['Authorization'] eq '%s' and `+
-						`http.request.headers['X-Request-ID'] eq '%s'`,
-					"Bearer "+faker.Jwt(), // Re-generate to match expectation, order matters
-					faker.UUIDHyphenated(),
-				),
-			},
-			{
-				name: "unsupported path type regex",
-				match: gatewayv1.HTTPRouteMatch{
-					Path: &gatewayv1.HTTPPathMatch{
-						Type:  lo.ToPtr(gatewayv1.PathMatchRegularExpression),
-						Value: lo.ToPtr("/users/[0-9]+"),
-					},
-				},
-				wantErrIs: errUnsupportedMatch,
-			},
-			{
-				name: "unsupported header type regex",
-				match: gatewayv1.HTTPRouteMatch{
-					Headers: []gatewayv1.HTTPHeaderMatch{
-						{
-							Type:  lo.ToPtr(gatewayv1.HeaderMatchRegularExpression),
-							Name:  "X-User-ID",
-							Value: "^[a-z]+$",
-						},
-					},
-				},
-				wantErrIs: errUnsupportedMatch,
-			},
-			{
-				name: "unsupported query param match",
-				match: gatewayv1.HTTPRouteMatch{
-					QueryParams: []gatewayv1.HTTPQueryParamMatch{
-						{
-							Type:  lo.ToPtr(gatewayv1.QueryParamMatchExact),
-							Name:  "page",
-							Value: "1",
-						},
-					},
-				},
-				wantErrIs: errUnsupportedMatch,
-			},
-			{
-				name: "unsupported method match",
-				match: gatewayv1.HTTPRouteMatch{
-					Method: lo.ToPtr(gatewayv1.HTTPMethodPost),
-				},
-				wantErrIs: errUnsupportedMatch,
-			},
-			{
-				name:  "no matches defined",
-				match: gatewayv1.HTTPRouteMatch{},
-				want:  "",
-			},
-			{
-				name: "nil path value",
-				match: gatewayv1.HTTPRouteMatch{
-					Path: &gatewayv1.HTTPPathMatch{
-						Type:  lo.ToPtr(gatewayv1.PathMatchExact),
-						Value: nil, // Invalid config, but test behavior
-					},
-				},
-				wantErrText: "path match value cannot be nil",
-			},
-			{
-				name: "nil path type",
-				match: gatewayv1.HTTPRouteMatch{
-					Path: &gatewayv1.HTTPPathMatch{
-						Type:  nil, // Invalid config
-						Value: lo.ToPtr("/test"),
-					},
-				},
-				// The implementation defaults the type to PathMatchPathPrefix if nil,
-				// so this test case should now expect a valid condition or be removed/updated.
-				// Let's update it to expect the default prefix match.
-				want: `http.request.url.path sw '/test'`,
-			},
-			{
-				name: "nil header type",
-				match: gatewayv1.HTTPRouteMatch{
-					Headers: []gatewayv1.HTTPHeaderMatch{
-						{
-							Type:  nil, // Invalid config
-							Name:  "X-Test",
-							Value: "value",
-						},
-					},
-				},
-				// The implementation defaults the type to HeaderMatchExact if nil.
-				// Update the test case to expect the default exact match.
-				// wantErrIs: errUnsupportedMatch, // Default type is Exact, but explicit is better. Treat nil as unsupported.
-				want: `http.request.headers['X-Test'] eq 'value'`,
-			},
 		}
 
-		for _, tc := range tests {
-			t.Run(tc.name, func(t *testing.T) {
-				// Need to reconstruct the specific random values for this test case if they were used in `want`
-				// This is a bit awkward with faker. Let's adjust the test case where faker was used directly.
-				if tc.name == "prefix path and multiple exact header matches" {
-					// Reconstruct the exact match object used to generate the `want` string
-					authValue := "Bearer " + faker.Jwt()
-					requestID := faker.UUIDHyphenated()
-					tc.match = gatewayv1.HTTPRouteMatch{
+		tests := []func() testCase{
+			func() testCase {
+				pathValue := "/" + faker.Word() + "/" + faker.Word()
+				return testCase{
+					name: "exact path match",
+					match: gatewayv1.HTTPRouteMatch{
+						Path: &gatewayv1.HTTPPathMatch{
+							Type:  lo.ToPtr(gatewayv1.PathMatchExact),
+							Value: lo.ToPtr(pathValue),
+						},
+					},
+					want: fmt.Sprintf(`http.request.url.path eq '%s'`, pathValue),
+				}
+			},
+			func() testCase {
+				pathPrefix := "/" + faker.Word() + "/" + faker.Word()
+				return testCase{
+					name: "prefix path match",
+					match: gatewayv1.HTTPRouteMatch{
+						Path: &gatewayv1.HTTPPathMatch{
+							Type:  lo.ToPtr(gatewayv1.PathMatchPathPrefix),
+							Value: lo.ToPtr(pathPrefix),
+						},
+					},
+					want: fmt.Sprintf(`http.request.url.path sw '%s'`, pathPrefix),
+				}
+			},
+			func() testCase {
+				headerName := "X-" + faker.Word()
+				headerValue := faker.UUIDHyphenated()
+				return testCase{
+					name: "exact header match",
+					match: gatewayv1.HTTPRouteMatch{
+						Headers: []gatewayv1.HTTPHeaderMatch{
+							{
+								Type:  lo.ToPtr(gatewayv1.HeaderMatchExact),
+								Name:  gatewayv1.HTTPHeaderName(headerName),
+								Value: headerValue,
+							},
+						},
+					},
+					want: fmt.Sprintf(`http.request.headers['%s'] eq '%s'`, headerName, headerValue),
+				}
+			},
+			func() testCase {
+				headerName1 := "X-" + faker.Word() + "-1"
+				headerValue1 := faker.Word()
+				headerName2 := "X-" + faker.Word() + "-2"
+				headerValue2 := faker.UUIDHyphenated()
+				return testCase{
+					name: "multiple exact header matches",
+					match: gatewayv1.HTTPRouteMatch{
+						Headers: []gatewayv1.HTTPHeaderMatch{
+							{
+								Type:  lo.ToPtr(gatewayv1.HeaderMatchExact),
+								Name:  gatewayv1.HTTPHeaderName(headerName1),
+								Value: headerValue1,
+							},
+							{
+								Type:  lo.ToPtr(gatewayv1.HeaderMatchExact),
+								Name:  gatewayv1.HTTPHeaderName(headerName2),
+								Value: headerValue2,
+							},
+						},
+					},
+					want: fmt.Sprintf(
+						`http.request.headers['%s'] eq '%s' and http.request.headers['%s'] eq '%s'`,
+						headerName1, headerValue1, headerName2, headerValue2,
+					),
+				}
+			},
+			func() testCase {
+				pathValue := "/" + faker.Word() + "/" + faker.Word()
+				headerName := "Content-Type"
+				headerValue := "application/" + faker.Word()
+				return testCase{
+					name: "exact path and exact header match",
+					match: gatewayv1.HTTPRouteMatch{
+						Path: &gatewayv1.HTTPPathMatch{
+							Type:  lo.ToPtr(gatewayv1.PathMatchExact),
+							Value: lo.ToPtr(pathValue),
+						},
+						Headers: []gatewayv1.HTTPHeaderMatch{
+							{
+								Type:  lo.ToPtr(gatewayv1.HeaderMatchExact),
+								Name:  gatewayv1.HTTPHeaderName(headerName),
+								Value: headerValue,
+							},
+						},
+					},
+					want: fmt.Sprintf(
+						`http.request.url.path eq '%s' and http.request.headers['%s'] eq '%s'`,
+						pathValue, headerName, headerValue,
+					),
+				}
+			},
+			func() testCase {
+				authValue := "Bearer " + faker.Jwt()
+				requestID := faker.UUIDHyphenated()
+				return testCase{
+					name: "prefix path and multiple exact header matches",
+					match: gatewayv1.HTTPRouteMatch{
 						Path: &gatewayv1.HTTPPathMatch{
 							Type:  lo.ToPtr(gatewayv1.PathMatchPathPrefix),
 							Value: lo.ToPtr("/api/v1"),
@@ -234,17 +140,123 @@ func TestOciLoadBalancerRuleSetsImpl(t *testing.T) {
 								Value: requestID,
 							},
 						},
-					}
-					tc.want = fmt.Sprintf(
-						"http.request.url.path sw '/api/v1' and "+
-							"http.request.headers['Authorization'] eq '%s' and "+
-							"http.request.headers['X-Request-ID'] eq '%s'",
+					},
+					want: fmt.Sprintf(
+						`http.request.url.path sw '/api/v1' and `+
+							`http.request.headers['Authorization'] eq '%s' and `+
+							`http.request.headers['X-Request-ID'] eq '%s'`,
 						authValue,
 						requestID,
-					)
+					),
 				}
+			},
+			func() testCase {
+				return testCase{
+					name: "unsupported path type regex",
+					match: gatewayv1.HTTPRouteMatch{
+						Path: &gatewayv1.HTTPPathMatch{
+							Type:  lo.ToPtr(gatewayv1.PathMatchRegularExpression),
+							Value: lo.ToPtr("/users/[0-9]+"),
+						},
+					},
+					wantErrIs: errUnsupportedMatch,
+				}
+			},
+			func() testCase {
+				return testCase{
+					name: "unsupported header type regex",
+					match: gatewayv1.HTTPRouteMatch{
+						Headers: []gatewayv1.HTTPHeaderMatch{
+							{
+								Type:  lo.ToPtr(gatewayv1.HeaderMatchRegularExpression),
+								Name:  "X-User-ID",
+								Value: "^[a-z]+$",
+							},
+						},
+					},
+					wantErrIs: errUnsupportedMatch,
+				}
+			},
+			func() testCase {
+				return testCase{
+					name: "unsupported query param match",
+					match: gatewayv1.HTTPRouteMatch{
+						QueryParams: []gatewayv1.HTTPQueryParamMatch{
+							{
+								Type:  lo.ToPtr(gatewayv1.QueryParamMatchExact),
+								Name:  "page",
+								Value: "1",
+							},
+						},
+					},
+					wantErrIs: errUnsupportedMatch,
+				}
+			},
+			func() testCase {
+				return testCase{
+					name: "unsupported method match",
+					match: gatewayv1.HTTPRouteMatch{
+						Method: lo.ToPtr(gatewayv1.HTTPMethodPost),
+					},
+					wantErrIs: errUnsupportedMatch,
+				}
+			},
+			func() testCase {
+				return testCase{
+					name:  "no matches defined",
+					match: gatewayv1.HTTPRouteMatch{},
+					want:  "",
+				}
+			},
+			func() testCase {
+				return testCase{
+					name: "nil path value",
+					match: gatewayv1.HTTPRouteMatch{
+						Path: &gatewayv1.HTTPPathMatch{
+							Type:  lo.ToPtr(gatewayv1.PathMatchExact),
+							Value: nil, // Invalid config, but test behavior
+						},
+					},
+					wantErrText: "path match value cannot be nil",
+				}
+			},
+			func() testCase {
+				pathValue := "/" + faker.Word() + "/" + faker.Word()
+				return testCase{
+					name: "nil path type",
+					match: gatewayv1.HTTPRouteMatch{
+						Path: &gatewayv1.HTTPPathMatch{
+							Type:  nil, // Invalid config
+							Value: lo.ToPtr(pathValue),
+						},
+					},
+					want: fmt.Sprintf(`http.request.url.path sw '%s'`, pathValue),
+				}
+			},
+			func() testCase {
+				headerName := "X-" + faker.Word()
+				headerValue := faker.Word()
+				return testCase{
+					name: "nil header type",
+					match: gatewayv1.HTTPRouteMatch{
+						Headers: []gatewayv1.HTTPHeaderMatch{
+							{
+								Type:  nil, // Invalid config
+								Name:  gatewayv1.HTTPHeaderName(headerName),
+								Value: headerValue,
+							},
+						},
+					},
+					want: fmt.Sprintf(`http.request.headers['%s'] eq '%s'`, headerName, headerValue),
+				}
+			},
+		}
 
-				actual, err := r.mapHTTPRouteMatchToCondition(tc.match)
+		for _, tcFunc := range tests {
+			tc := tcFunc()
+			t.Run(tc.name, func(t *testing.T) {
+				rs := newOciLoadBalancerRuleSets()
+				actual, err := rs.mapHTTPRouteMatchToCondition(tc.match)
 
 				switch {
 				case tc.wantErrIs != nil:
@@ -253,7 +265,6 @@ func TestOciLoadBalancerRuleSetsImpl(t *testing.T) {
 					require.ErrorContains(t, err, tc.wantErrText)
 				default:
 					require.NoError(t, err)
-					// Normalize whitespace for comparison as OCI might be flexible
 					assert.Equal(t, strings.Join(strings.Fields(tc.want), " "), strings.Join(strings.Fields(actual), " "))
 				}
 			})
